51cd34f30d6c5415a42725d007aaee763bfc6e98
Hrithikrana377 <hrithikranaji@gmail.com>
2021-07-04 12:21:53 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {useEffect, useState} from 'react';
+import Player from './components/Player';
 function App() {
+  const [songs] = useState([
+    {
+      title: "1400/999",
+      artist: "juice and red",
+      img_src: "./images/1400999.png",
+      src: "./music/1400 999.mp4",
+    },
+    {
+      title: "After party",
+      artist: "Don Toliver",
+      img_src: "./images/after party.jpeg",
+      src: "./music/After-Party.mp3",
+    },
+    {
+      title: "conversations",
+      artist: "Juice wrld",
+      img_src: "./images/conversation.png",
+      src: "./music/Conversation.mp4",
+    },
+    {
+      title: "Ransom",
+      artist: "Lil tecca",
+      img_src: "./images/ransom.jpeg",
+      src: "./music/Ransom.mp4",
+    },
+  ]);
+
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [nextSongIndex, setNextSongIndex] = useState(currentSongIndex + 1);
+
+  useEffect(()=>{
+    setNextSongIndex(() =>{
+      if (currentSongIndex + 1 > songs.length - 1){
+        return 0;
+      } else{
+        return currentSongIndex + 1;
+      }
+    })
+  },[currentSongIndex]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Player 
+        currentSongIndex={currentSongIndex}
+        setCurrentSongIndex={setCurrentSongIndex}
+        nextSongIndex={nextSongIndex}
+        songs={songs}
+      />
     </div>
   );
 }
